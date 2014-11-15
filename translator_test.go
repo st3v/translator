7773f02fce3e6ea562e1e172cf18025faffb8c7b
Stev Witzel <stev.witzel@gmail.com>
2014-11-15 00:33:24 -0800

    Add basic interface test

---
+++ translator_test.go
@@ -0,0 +1,19 @@
+package translator
+
+import "testing"
+
+// Make sure nobody breaks the interface.
+func TestTranslatorInterface(t *testing.T) {
+	var translator Translator = &testTranslator{}
+	translator.Translate("", "", "")
+}
+
+type testTranslator struct{}
+
+func (t *testTranslator) Languages() ([]Language, error) {
+	return nil, nil
+}
+
+func (t *testTranslator) Translate(text, from, to string) (string, error) {
+	return "", nil
+}
