2eef9d60efea884813bca71d2f1fad8a9450db9a
Stev Witzel <stev.witzel@gmail.com>
2015-01-24 01:53:36 +0000

    Add authenticator for Google Translate API

---
+++ google/authenticator.go
@@ -0,0 +1,25 @@
+package google
+
+import (
+	"net/http"
+
+	_http "github.com/st3v/translator/http"
+)
+
+type authenticator struct {
+	apiKey string
+}
+
+// NewAuthenticator instantiates a new Authenticator for Google's Translate API.
+func NewAuthenticator(apiKey string) _http.Authenticator {
+	return &authenticator{
+		apiKey: apiKey,
+	}
+}
+
+func (a *authenticator) Authenticate(request *http.Request) error {
+	params := request.URL.Query()
+	params.Add("key", a.apiKey)
+	request.URL.RawQuery = params.Encode()
+	return nil
+}
---
+++ google/authenticator_test.go
@@ -0,0 +1,44 @@
+package google
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestAuthenticate(t *testing.T) {
+	apiKey := "my-secret-api-key"
+	authenticator := NewAuthenticator(apiKey)
+
+	host := "localhost:6666"
+	params := "one=1&two=2"
+	originalURL := fmt.Sprintf("%s?%s", host, params)
+
+	request, err := http.NewRequest("GET", originalURL, nil)
+	if err != nil {
+		t.Errorf("Unexpected error creating new request: %s", err.Error())
+	}
+
+	if request.URL.String() != originalURL {
+		t.Errorf(
+			"Unexpected request URL prior to authentication. Got: %s. Want: %s.",
+			request.URL.String(),
+			originalURL,
+		)
+	}
+
+	err = authenticator.Authenticate(request)
+	if err != nil {
+		t.Errorf("Unexpected error authenticating request: %s", err.Error())
+	}
+
+	newURL := fmt.Sprintf("%s?key=%s&%s", host, apiKey, params)
+	if request.URL.String() != newURL {
+		t.Errorf(
+			"Unexpected request URL prior to authentication. Got: %s. Want: %s.",
+			request.URL.String(),
+			newURL,
+		)
+	}
+
+}
